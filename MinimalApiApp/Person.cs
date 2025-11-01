9859275f68260c76c64c7ea4d6799bf4855db95a
giftos1 <giftsydney676@gmail.com>
2025-11-02 00:07:25 +0800

    Create a person class

---
+++ MinimalApiApp/Person.cs
@@ -0,0 +1,15 @@
+﻿namespace MinimalApiApp
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Person(string firstName, string lastName) {
+        
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+    }
+}
