82d8ed51b3d95d2355b3c9f80a21b718849a3c1b
giftos1 <giftsydney676@gmail.com>
2025-11-02 01:43:04 +0800

    add new handler class to replace and add fruit

---
+++ MinimalApiApp/Handlers.cs
@@ -0,0 +1,17 @@
+﻿namespace MinimalApiApp
+{
+    class Handlers
+    {
+        // instance method
+        public void ReplaceFruit(string id, Fruit fruit)
+        {
+            Fruit.All[id] = fruit;
+        }
+
+        // convert response to JSON object
+        public static void AddFruit(string id, Fruit fruit)
+        {
+            Fruit.All.Add(id, fruit);
+        }
+    }
+}
