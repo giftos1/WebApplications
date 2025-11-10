a851366bcd36fa663752f1573908792d1cb6786a
giftos1 <giftsydney676@gmail.com>
2025-11-10 23:26:06 +0800

    Create public record with Data Annotations

--- ValidationApp1/Program.cs
+++ ValidationApp1/Program.cs
@@ -1,4 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+app.MapPost("/user", (UserModel user) => user.ToString());
+
 app.Run();
+
+public record UserModel
+{
+    [Required]
+    [StringLength(100)]
+    [Display(Name = "Your name")]
+    public string FirstName { get; set; }
+
+    [Required]
+    [StringLength(100)]
+    [Display(Name = "Last name")]
+    public string LastName { get; set; }
+
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+
+    [Phone]
+    [Display(Name = "Phone number")]
+    public string PhoneNumber { get; set; }
+
+}
